8731df5e946f097cba5a2d26229f762e3110f338
cym2050 <1390062269@qq.com>
2020-07-29 10:16:53 +0800

    Code Review_user status
    Clean up the status of the previous user when different users log in

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,23 +28,25 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '0',
+    name: 'john',
+    email: 'john@gmail.com',
+    entries: 0,
+    joined: new Date()
+  }
+}
+
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '0',
-        name: 'john',
-        email: 'john@gmail.com',
-        entries: 0,
-        joined: new Date()
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -103,7 +105,7 @@ class App extends React.Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
