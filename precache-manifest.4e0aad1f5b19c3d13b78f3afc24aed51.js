48895d6e81844d0b6100d11ee5b24aed84d9700a
cym2050 <1390062269@qq.com>
2020-08-23 20:15:46 +0800

    Updates

---
+++ precache-manifest.4e0aad1f5b19c3d13b78f3afc24aed51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8f8c49fc8fc244adba37ddd3f1c50c",
+    "url": "./bu/index.html"
+  },
+  {
+    "revision": "05bc02a5f01a81790ce0",
+    "url": "./bu/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d540adadac8c49e77660",
+    "url": "./bu/static/css/main.347534d6.chunk.css"
+  },
+  {
+    "revision": "05bc02a5f01a81790ce0",
+    "url": "./bu/static/js/2.dc8db205.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./bu/static/js/2.dc8db205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d540adadac8c49e77660",
+    "url": "./bu/static/js/main.6baf1aec.chunk.js"
+  },
+  {
+    "revision": "851fe037f8a3e5f2a2f1",
+    "url": "./bu/static/js/runtime-main.6f2a1501.js"
+  }
+]);
